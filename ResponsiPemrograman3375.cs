832fc3bacdcbea3e65ec9a1e3354c976108ac6e1
JustRams <akbarpramanaputra062@students.amikom.ac.id>
2021-04-13 14:55:48 +0700

    Responsi Penrograman 20.11.3375

---
+++ ResponsiPemrograman3375.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Task;
+
+
+namespace ResponsiPemrograman3375
+{
+	public class Responsi
+	{
+		public string Nama { get; set; }
+		public string Gaji { get; set; }
+		
+		public void Data()
+		{
+			Console.Writeline("Silahkan mengisi Nama dan Gaji :)");
+			Console.Writeline("Nama: {0}", Nama);
+			Console.Writeline("Gaji: {0}", Gaji);
+			Console.Writeline("------------------");
+		}
+		
+		public void KenaikanGaji
+		{
+			
+		}
+	
+	}
+}
